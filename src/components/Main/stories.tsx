325e8b3ff202620d469357368195a71ff91710cc
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-02-01 19:04:34 -0300

    refactor: remove duplication code

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,11 +3,7 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  args: {
-    title: 'title default',
-    description: 'description default'
-  }
+  component: Main
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
@@ -15,5 +11,3 @@ Basic.args = {
   title: 'title basic',
   description: 'description basic'
 }
-
-export const Default: Story = (args) => <Main {...args} />
